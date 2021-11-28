4c616727222db5885b3b1d308374f39aa927f081
Suman Raj Sharma <sumanrajsharma@dristis-mbp.lan>
2021-11-29 10:49:11 +1100

    importing topic modelling module

--- web_dashboard.py
+++ web_dashboard.py
@@ -6,7 +6,8 @@ import pickle
 import os
 import pandas as pd
 
-
+#from decouple import config
+from topicmapping import topicmapping as tm
 
 if __name__ == '__main__':
 	st.title('Streamlit run...')
