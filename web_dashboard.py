0fab3891e10d75fde1f61635c3c819019acb71d6
Suman Raj Sharma <sumanrajsharma@dristis-mbp.lan>
2021-11-29 10:44:28 +1100

    setting up for Streamlit cloud

---
+++ web_dashboard.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests
+import time
+import webbrowser
+import pickle
+import os
+import pandas as pd
+
+
+
+
+if __name__ == '__main__':
+	st.title('Streamlit run...')
